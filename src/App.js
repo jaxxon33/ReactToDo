239620346090f9878ec4da2201a98640559b4cb1
Paul Young <paul@MacMini.local>
2019-01-14 21:29:37 +1100

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Todos from './Todos';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Take rubbish',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Take garbage',
+        completed: false
+      }
+    ]
+  }
   render() {
+    // console.log(this.state.todos)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
 }
 
-export default App;
+
---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+        <p>{this.props.todo.title}</p>
+      </div>
+    )
+  }
+}
